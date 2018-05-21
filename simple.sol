9dcad903bc0326f618e8b31c10ee2abf2526a59c
loyyoung <39472756+loyyoung@users.noreply.github.com>
2018-05-21 16:28:56 +0900

    수정

---
+++ simple.sol
@@ -0,0 +1,25 @@
+﻿pragma solidity ^0.4.0; 
+
+ 
+
+contract simdata {
+
+    uint storedData;
+
+ 
+
+    function set(uint x) {
+
+        storedData = x;
+
+    }
+
+ 
+
+    function get() constant returns (uint) {
+
+        return storedData;
+
+    }
+
+}
